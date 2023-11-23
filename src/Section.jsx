861f3e8e7ebcfd985bd7cbaaeb530c10d5b089a8
Erik <eait84344@gmail.com>
2023-11-23 22:29:06 +0400

    main category is read

--- src/Section.jsx
+++ src/Section.jsx
@@ -14,6 +14,42 @@ export default function Section() {
 
         <img src="https://want.am/images/promo/14/111.jpeg" alt="main img" />
       </div>
+
+      <div className="main-category_container">
+        <div className="main-category">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">
+              <img src="https://want.am/images/thumbnails/70/70/detailed/5/68292-1.jpeg.webp" alt="man" />
+
+              <div className="about">
+                <h2>Կանանց նվերներ</h2>
+                <p>216 իր</p>
+                <p>Սկսած <span>3900ԴՐ</span></p>
+              </div>
+            </div>
+
+            <div className="swiper-slide">
+              <img src="https://want.am/images/thumbnails/70/70/detailed/2/71102-1.jpeg.webp" alt="man" />
+
+              <div className="about">
+                <h2>Տղամարդկանց</h2>
+                <p>308 իր</p>
+                <p>Սկսած <span>3000ԴՐ</span></p>
+              </div>
+            </div>
+
+            <div className="swiper-slide">
+              <img src="https://want.am/images/thumbnails/70/70/detailed/6/U0437307_big.jpeg.webp" alt="man" />
+
+              <div className="about">
+                <h2>Հոբբի</h2>
+                <p>1391 իր</p>
+                <p>Սկսած <span>3900ԴՐ</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
