6d91c5a1b9999dcc6dc2b59bb1c9da72840b7f83
Erik <eait84344@gmail.com>
2023-11-23 23:14:44 +0400

    home page is read

--- src/Section.jsx
+++ src/Section.jsx
@@ -50,6 +50,395 @@ export default function Section() {
           </div>
         </div>
       </div>
+
+      <div className="homepage-container">
+        <div className="homepage">
+          <h2>
+            Պահանջված
+          </h2>
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/2/HTB15qwGHFXXXXXqXFXXq6xXFXXXU.jpg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>USB ֆլեշ կրիչ կիթառի տեսքով</p>
+                <p id='price'>7900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/7/dino000.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սենսորային սիլիկոնե լուսամփոփ «Դինոզավր»</p>
+                <p id='price'>10900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/13/700-nw_xvau-na.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սթորմգլաս բյուրեղի տեսքով</p>
+                <p id='price'>13500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/3/HTB1uqJxRFXXXXb2XVXXq6xXFXXXC_wi0h-45.jpg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սենսորային լուսամփոփ «Նապաստակ»</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <h2>
+            ԲՈԼՈՐ ՆՎԵՐՆԵՐԸ
+          </h2>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row-fluid">
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/sovetskaya_podvodnaya_lodka_shchuka.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Հավաքովի մոդել «Խորհրդային սուզանավ «Շչուկա»</p>
+                <p id='price'>11500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_12.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Արևային էներգիայով արտաքին մարտկոց</p>
+                <p id='price'>11900ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA_%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2023-11-12_%D0%B2_14.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Լիցքավորման սարք-ժամացույց-լուսամփոփ Square</p>
+                <p id='price'>25500ԴՐ</p>
+              </div>
+            </div>
+
+            <div className="box">
+              <img src="https://want.am/images/thumbnails/400/400/detailed/14/g92250482524p.jpeg.webp" alt="" />
+
+              <div className="textContent">
+                <span>TOP</span>
+                <p>Սեղանի խաղ «Նորդգարդ. Նոր հողեր»</p>
+                <p id='price'>44400ԴՐ</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
